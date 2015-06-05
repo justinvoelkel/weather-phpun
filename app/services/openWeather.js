eced1326eda4982e250ba0913d8a41a7004948fe
Justin Voelkel <justin@budgetdumpster.com>
2015-06-05 12:43:36 -0400

    Weather Request -form request based on forecast selected

--- app/services/openWeather.js
+++ app/services/openWeather.js
@@ -5,8 +5,17 @@ var openWeather = angular.module('OpenWeatherSrvc',[]);
 
 openWeather.factory('Weather', ['$http', function($http){
     return {
-        request: function(geocode) {
-            var request = "http://api.openweathermap.org/data/2.5/weather?lat="+geocode.lat+"&lon="+geocode.lng+"&units=imperial";
+        request: function(geocode,length) {
+
+            var request = "http://api.openweathermap.org/data/2.5/";
+            //form request based on forecast length
+            if(length>0){
+                request = request+"forecast/daily?lat="+geocode.lat+"&lon="+geocode.lng+"&cnt="+length+"&mode=json";
+            } else {
+                request = request+"weather?lat="+geocode.lat+"&lon="+geocode.lng;
+            }
+            //set correct unit of measure
+            request = request+"&units=imperial";
             var request = $http.get(request);
             return request;
         }
